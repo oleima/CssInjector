c9f02f5bd4c064f73d9736f6f8d47470b2788870
Ole Martin Gjerde <ole.martin.gjerde@goodtech.no>
2024-04-03 16:44:02 +0200

    feat: added to js the ability to find the correct elements and set hardcoded values for width

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
 // Get The URL
-const site = window.location.hostname
+const site = window.location.hostname;
 
-alert("Injector - The JavaScript has been injected to: " + site + " 🤖")
+// Running every interval
+setInterval(function(){
+  var loaded = false;
+  var elementList = document.getElementsByClassName("x-component u4-iframecontainer x-box-item x-component-default");
+
+  if(elementList.length>0){
+    var childrenForDivContainingIframe = elementList[0].children
+    if(childrenForDivContainingIframe.length > 0){
+
+      var iframe = elementList[0].children[0];
+  
+      var frame = iframe.contentWindow.document.getElementById("contentContainerFrame");
+    
+      var tables = frame.contentWindow.document.getElementsByClassName("Excel ColumnsMovable ColumnsResizable ColumnsAutoResizable");
+      if(tables.length>1){
+        loaded = true;
+        var table = tables[1];
+    
+        table.setAttribute("style", "border-width: 0px; border-collapse: separate; width: 1400px;"); 
+      
+        var beskrivelser = frame.contentWindow.document.getElementsByClassName("GridCell");
+      
+        var beskrivelse;
+        for (let i = 0; i < beskrivelser.length; i++) {
+          if(beskrivelser[i].id.startsWith("b_s89_g89s90_ctl02__headerdescription")){
+            beskrivelse = beskrivelser[i]
+          }
+        }
+      
+        beskrivelse.setAttribute("style", "white-space:nowrap;text-align:center;vertical-align:middle;cursor:pointer;width:150px;"); 
+      }
+    }
+  }
+  if(!loaded){
+    console.log("Elements not loaded yet")
+  }
+
+}, 1000);
